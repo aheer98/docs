7d63d4b7cd5a02e519dd2f1673f4524c1a68e9f4
a.heer <a.heer@outlook.com>
2022-03-07 22:23:57 -0500

    Updates

--- dgii/qrcode.rb
+++ dgii/qrcode.rb
@@ -1,4 +1,20 @@
-require 'qrcode'
+require 'rqrcode'
 
+url    = 'https://aheer98.github.io/docs/cppp.aspx.html'
 
-qrcode = RQRCode::QRCode.new("file:///home/aheer/Workspace/arena/dgii/cppp.aspx")
+qrcode = RQRCode::QRCode.new(url)
+
+png = qrcode.as_png(
+  bit_depth: 1,
+  border_modules: 4,
+  color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+  color: "black",
+  file: nil,
+  fill: "white",
+  module_px_size: 6,
+  resize_exactly_to: false,
+  resize_gte_to: false,
+  size: 512
+)
+
+IO.binwrite("qrcodes/qrcode.#{ Time.now.to_i }.png", png.to_s)
